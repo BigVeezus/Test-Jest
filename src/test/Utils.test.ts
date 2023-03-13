3bca4570ab09d0fb07a1a870859949620e7c41b1
Elvis O <elvis.osujic@gmail.com>
2023-03-13 20:37:29 +0100

    multiple test structure

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,44 @@
-import { toUpperCase } from "../app/utils";
+import { getStringInfo, toUpperCase } from "../app/utils";
 
 describe("Utils test suite", () => {
-  test("should return uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  it("should return uppercase", () => {
+    // arrange:
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    // act:
+    const actual = sut("abc");
+
+    // assert:
+    expect(actual).toBe(expected);
+  });
+
+  it.only("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toStrictEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["-", "S", "t", "r", "i", "n", "g", "M", "y"])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
