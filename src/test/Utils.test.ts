bea2892abdd78c19cafe17e2acfbaec550335a6e
Elvis O <elvis.osujic@gmail.com>
2023-03-14 05:13:35 +0100

    perimeterized testing

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -13,32 +13,58 @@ describe("Utils test suite", () => {
     expect(actual).toBe(expected);
   });
 
-  it.only("should return info for valid string", () => {
-    const actual = getStringInfo("My-String");
-
-    expect(actual.lowerCase).toBe("my-string");
-    expect(actual.extraInfo).toStrictEqual({});
-
-    expect(actual.characters.length).toBe(9);
-    expect(actual.characters).toHaveLength(9);
-
-    expect(actual.characters).toEqual([
-      "M",
-      "y",
-      "-",
-      "S",
-      "t",
-      "r",
-      "i",
-      "n",
-      "g",
-    ]);
-
-    expect(actual.characters).toEqual(
-      expect.arrayContaining(["-", "S", "t", "r", "i", "n", "g", "M", "y"])
-    );
-
-    expect(actual.extraInfo).not.toBe(undefined);
-    expect(actual.extraInfo).toBeTruthy();
+  describe.only("ToUpperCase examples", () => {
+    it.each([
+      { input: "abc", expected: "ABC" },
+      { input: "My-String", expected: "MY-STRING" },
+      { input: "def", expected: "DEF" },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = toUpperCase(input);
+      expect(actual).toBe(expected);
+    });
   });
+
+  describe("getStringInfo for arg My-String should", () => {
+    test("return right length", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.characters).toHaveLength(9);
+    });
+    test("return lower case", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.lowerCase).toBe("my-stringa");
+    });
+    test("return uppercase", () => {
+      const actual = getStringInfo("My-String");
+      expect(actual.upperCase).toBe("MY-STRING");
+    });
+  });
+
+  // it.only("should return info for valid string", () => {
+  //   const actual = getStringInfo("My-String");
+
+  //   expect(actual.lowerCase).toBe("my-string");
+  //   expect(actual.extraInfo).toStrictEqual({});
+
+  //   expect(actual.characters.length).toBe(9);
+  //   expect(actual.characters).toHaveLength(9);
+
+  //   expect(actual.characters).toEqual([
+  //     "M",
+  //     "y",
+  //     "-",
+  //     "S",
+  //     "t",
+  //     "r",
+  //     "i",
+  //     "n",
+  //     "g",
+  //   ]);
+  //   expect(actual.characters).toContain<string>("y");
+  //   expect(actual.characters).toEqual(
+  //     expect.arrayContaining(["-", "S", "t", "r", "i", "n", "g", "M", "y"])
+  //   );
+
+  //   expect(actual.extraInfo).not.toBe(undefined);
+  //   expect(actual.extraInfo).toBeTruthy();
+  // });
 });
